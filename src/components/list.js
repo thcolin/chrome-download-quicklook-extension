6ba75e92316cc9480c403360e47425518ece479f
thcolin <th.colin3@gmail.com>
2018-02-12 20:29:18 +0100

    Fix sticky toolbar 🍯

--- src/components/list.js
+++ src/components/list.js
@@ -1,3 +1,4 @@
+import { css } from 'glamor'
 import card from 'components/card'
 const html = require('choo/html')
 
@@ -5,9 +6,17 @@ export default function list(state, emit) {
   const regex = new RegExp(state.input, 'gi')
   const results = state.items.result
     .filter(id => !state.input || state.items.entities[id].url.match(regex) || state.items.entities[id].filename.split('/').pop().match(regex))
+  const styles = {
+    list: css({
+      width: '400px',
+      minHeight: '300px',
+      maxHeight: '500px',
+      overflow: 'scroll'
+    })
+  }
 
   return html`
-    <div>
+    <div className=${styles.list}>
       ${results.length ? results.map(id => card(id, state, emit)) : 'placeholder'}
     </div>
   `
